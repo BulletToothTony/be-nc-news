8e0ed54b1cf5f947c00363680e9603d76941b540
Henry Westhoff-Lewis <henry@Henrys-MacBook-Pro-2.local>
2024-01-15 13:41:07 +0000

    Task 3 get /api complete

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,4 @@
-const { getTopics } = require("../models/model")
+const { getTopics, getEndpoints } = require("../models/model")
 
 exports.getAllTopics = async (req, res) => {
     try {
@@ -7,6 +7,18 @@ exports.getAllTopics = async (req, res) => {
         res.status(200).send(allTopics)
     }
 
+    catch(err) {
+        console.log(err)
+    }
+}
+
+
+exports.getApiEndpoints = async (req, res) => {
+    try {
+        const apiEndpoints = await getEndpoints()
+
+        res.status(200).send(apiEndpoints)
+    }
     catch(err) {
         console.log(err)
     }
